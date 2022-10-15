e2477941fac617b22d8529166c5ae9e6c10ad96e
Vitalii Psl <vitalii.psl.main@gmail.com>
2022-10-15 12:59:38 +0300

    Add User repository

---
+++ src/main/java/com/example/flashcards/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.example.flashcards.repository;
+
+import com.example.flashcards.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+}
