d7a65ee28f250c593051e7582736082c250e1285
Vitalii Psl <vitalii.psl.main@gmail.com>
2022-10-15 15:45:31 +0300

    Implement user details

--- src/main/java/com/example/flashcards/model/User.java
+++ src/main/java/com/example/flashcards/model/User.java
@@ -1,7 +1,12 @@
 package com.example.flashcards.model;
 
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import javax.persistence.*;
+import java.util.Collection;
+import java.util.List;
 import java.util.Objects;
 import lombok.Data;
 
@@ -11,7 +16,7 @@ import lombok.Data;
         @Index(name = "idx_user_username", columnList = "username", unique = true),
         @Index(name = "idx_user_email", columnList = "email", unique = true)
 })
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
@@ -22,6 +27,8 @@ public class User {
     @Column(nullable = false)
     private String email;
     private String password;
+
+    private boolean enabled;
 
     @Override
     public boolean equals(Object o) {
@@ -34,5 +41,40 @@ public class User {
     @Override
     public int hashCode() {
         return Objects.hash(email);
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
     }
 }
