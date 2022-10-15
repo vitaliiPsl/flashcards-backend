59591ab40c74aa28eaeb39278884871d6101c0a8
Vitalii Psl <vitalii.psl.main@gmail.com>
2022-10-15 11:48:40 +0300

    Init commit

---
+++ src/main/java/com/example/flashcards/FlashcardsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.flashcards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashcardsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashcardsBackendApplication.class, args);
+	}
+
+}
