93e3a92132af5ac767ef03a938f1bef00c45c73b
Vitalii Psl <vitalii.psl.main@gmail.com>
2022-10-15 15:52:02 +0300

    Create authentication request and response payloads

---
+++ src/main/java/com/example/flashcards/dto/requests/AuthRequest.java
@@ -0,0 +1,20 @@
+package com.example.flashcards.dto.requests;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthRequest {
+    @Email(message = "Email address must be valid")
+    @NotBlank(message = "Email address is required")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+}
