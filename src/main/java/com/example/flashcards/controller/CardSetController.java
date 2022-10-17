df90a2ae6135fe4385266dc323f552214df77bf3
Vitalii Psl <vitalii.psl.main@gmail.com>
2022-10-17 23:32:57 +0300

    Implement CardSet controller

---
+++ src/main/java/com/example/flashcards/controller/CardSetController.java
@@ -0,0 +1,38 @@
+package com.example.flashcards.controller;
+
+import com.example.flashcards.dto.CardSetDto;
+import com.example.flashcards.service.CardSetService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/sets")
+public class CardSetController {
+    private final CardSetService cardSetService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public CardSetDto saveSet(@RequestBody @Valid CardSetDto cardSetDto, Authentication authentication) {
+        return cardSetService.saveSet(cardSetDto, authentication);
+    }
+
+    @PutMapping("{id}")
+    public CardSetDto replaceSet(@PathVariable long id, @RequestBody @Valid CardSetDto cardSetDto, Authentication authentication) {
+        return cardSetService.replaceSet(id, cardSetDto, authentication);
+    }
+
+    @DeleteMapping("{id}")
+    public void deleteSet(@PathVariable long id, Authentication authentication) {
+        cardSetService.deleteSet(id, authentication);
+    }
+
+    @GetMapping("{id}")
+    public CardSetDto getSet(@PathVariable long id, Authentication authentication) {
+        return cardSetService.getSetById(id, authentication);
+    }
+}
