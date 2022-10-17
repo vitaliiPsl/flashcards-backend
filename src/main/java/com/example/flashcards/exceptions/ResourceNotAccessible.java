7a5d8eed095944516b7f675d665709c081920408
Vitalii Psl <vitalii.psl.main@gmail.com>
2022-10-17 23:32:04 +0300

    Create ResourceNotAccessible exception for the cases when user has no access to the resource

--- src/main/java/com/example/flashcards/exceptions/ResourceNotAccessible.java
+++ src/main/java/com/example/flashcards/exceptions/ResourceNotAccessible.java
@@ -5,7 +5,11 @@ import com.example.flashcards.model.User;
 public class ResourceNotAccessible extends RuntimeException {
 
     public ResourceNotAccessible(Object identifier, User user, Class<?> type) {
-        super(String.format("User %s has no access to %s with identifier: '%s'",
+        super(String.format("User %s has no access to resource %s with identifier: '%s'",
                 user.getId(), type.getSimpleName(), identifier));
+    }
+
+    public ResourceNotAccessible(Object identifier, Class<?> type) {
+        super(String.format("Resource %s is with identifier %s is no accessible for unauthorised users", type.getSimpleName(), identifier));
     }
 }
