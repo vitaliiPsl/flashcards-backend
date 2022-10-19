4302c0ba79e2e662c3a42f2d4f15123b80a9106c
Vitalii Psl <vitalii.psl.main@gmail.com>
2022-10-19 20:13:56 +0300

    Create CardController

---
+++ src/main/java/com/example/flashcards/controller/CardController.java
@@ -0,0 +1,44 @@
+package com.example.flashcards.controller;
+
+import com.example.flashcards.dto.CardDto;
+import com.example.flashcards.service.CardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/sets/{setId}/cards")
+public class CardController {
+
+    private final CardService cardService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public CardDto saveCard(@PathVariable long setId, @RequestBody @Valid CardDto cardDto, Authentication auth) {
+        return cardService.saveCard(setId, cardDto, auth);
+    }
+
+    @DeleteMapping("{cardId}")
+    public void deleteCard(@PathVariable long cardId, @PathVariable long setId, Authentication auth) {
+        cardService.deleteCard(cardId, setId, auth);
+    }
+
+    @PutMapping("{cardId}")
+    public CardDto replaceCard(
+            @PathVariable long cardId,
+            @PathVariable long setId,
+            @RequestBody @Valid CardDto cardDto,
+            Authentication auth
+    ) {
+        return cardService.replaceCard(cardId, setId, cardDto, auth);
+    }
+
+    @GetMapping("{cardId}")
+    public CardDto getCard(@PathVariable long cardId, @PathVariable long setId, Authentication auth) {
+        return cardService.getCardById(cardId, setId, auth);
+    }
+}
