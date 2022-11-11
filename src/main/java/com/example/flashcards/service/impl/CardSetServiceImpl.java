6c1077c4454e97d225d9fce0dc0e10c6197d5161
Vitalii Psl <vitalii.psl.main@gmail.com>
2022-11-11 23:02:19 +0200

    Refactor CardSet service and add logging

--- src/main/java/com/example/flashcards/service/impl/CardSetServiceImpl.java
+++ src/main/java/com/example/flashcards/service/impl/CardSetServiceImpl.java
@@ -5,10 +5,8 @@ import com.example.flashcards.dto.CardSetDto;
 import com.example.flashcards.exceptions.ResourceAlreadyExist;
 import com.example.flashcards.exceptions.ResourceNotAccessible;
 import com.example.flashcards.exceptions.ResourceNotFound;
-import com.example.flashcards.model.Card;
-import com.example.flashcards.model.CardSet;
-import com.example.flashcards.model.SetType;
-import com.example.flashcards.model.User;
+import com.example.flashcards.model.*;
+import com.example.flashcards.model.learning.Difficulty;
 import com.example.flashcards.repository.CardSetRepository;
 import com.example.flashcards.repository.UserRepository;
 import com.example.flashcards.service.CardSetService;
@@ -36,12 +34,15 @@ public class CardSetServiceImpl implements CardSetService {
     private final DtoMappers mappers;
 
     @Override
-    public CardSetDto saveSet(CardSetDto cardSetDto, Authentication authentication) {
-        User user = getUser(authentication);
+    public CardSetDto saveSet(CardSetDto cardSetDto, Authentication auth) {
+        log.info("Create set: {}", cardSetDto);
+
+        User user = getUser(auth);
         String setName = cardSetDto.getName();
 
         Optional<CardSet> existing = cardSetRepository.findByNameAndAuthor(setName, user);
         if (existing.isPresent()) {
+            log.error("User {} already has set with name '{}'", user, setName);
             throw new ResourceAlreadyExist(setName, CardSet.class);
         }
 
@@ -52,45 +53,46 @@ public class CardSetServiceImpl implements CardSetService {
         Set<Card> cards = mapCards(set, cardSetDto.getCards());
         set.setCards(cards);
 
-        CardSet saved = cardSetRepository.save(set);
-        return mappers.mapCardSetToCardSetDto(saved);
+        set = cardSetRepository.save(set);
+        return mappers.mapCardSetToCardSetDto(set);
     }
 
     @Override
-    public void deleteSet(long id, Authentication authentication) {
-        CardSet set = getSetAndVerifyAuthor(id, authentication);
+    public void deleteSet(long id, Authentication auth) {
+        log.info("Delete set with id {}", id);
+
+        CardSet set = getSetAndVerifyAuthor(id, auth);
 
         cardSetRepository.delete(set);
     }
 
     @Override
-    public CardSetDto replaceSet(long id, CardSetDto cardSetDto, Authentication authentication) {
-        CardSet existingSet = getSetAndVerifyAuthor(id, authentication);
+    public CardSetDto replaceSet(long id, CardSetDto cardSetDto, Authentication auth) {
+        log.info("Replace set with id {}", id);
 
-        CardSet set = mappers.mapCardSetDtoToCardSet(cardSetDto);
-        set.setId(id);
-        set.setAuthor(existingSet.getAuthor());
-        set.setCreatedAt(existingSet.getCreatedAt());
-        set.setUpdatedAt(LocalDateTime.now());
+        CardSet existing = getSetAndVerifyAuthor(id, auth);
+        existing.setName(cardSetDto.getName());
+        existing.setDescription(cardSetDto.getDescription());
+        existing.setType(cardSetDto.getType());
+        existing.setUpdatedAt(LocalDateTime.now());
 
-        Set<Card> cards = mapCards(set, cardSetDto.getCards());
-        set.setCards(cards);
-
-        cardSetRepository.save(set);
-        return mappers.mapCardSetToCardSetDto(set);
+        return mappers.mapCardSetToCardSetDto(existing);
     }
 
     @Override
-    public CardSetDto getSetById(long id, Authentication authentication) {
+    public CardSetDto getSetById(long id, Authentication auth) {
+        log.info("Get set with id {}", id);
 
         Optional<CardSet> optionalSet = cardSetRepository.findById(id);
         if (optionalSet.isEmpty()) {
+            log.error("Set with id {} doesn't exist", id);
             throw new ResourceNotFound(id, CardSet.class);
         }
-
-        User user = getUser(authentication);
         CardSet set = optionalSet.get();
+
+        User user = getUser(auth);
         if (!set.getAuthor().equals(user) && set.getType() == SetType.PRIVATE) {
+            log.error("User with id {} has no access to set {}", user.getId(), id);
             throw new ResourceNotAccessible(id, user, CardSet.class);
         }
 
@@ -124,13 +126,14 @@ public class CardSetServiceImpl implements CardSetService {
     private Card mapCard(CardSet cardSet, CardDto cardDto) {
         Card card = mappers.mapCardDtoToCard(cardDto);
         card.setSet(cardSet);
+        card.setDifficulty(Difficulty.HARD);
         card.setCreatedAt(LocalDateTime.now());
 
         return card;
     }
 
-    private CardSet getSetAndVerifyAuthor(long id, Authentication authentication) {
-        User user = getUser(authentication);
+    private CardSet getSetAndVerifyAuthor(long id, Authentication auth) {
+        User user = getUser(auth);
 
         Optional<CardSet> optionalCardSet = cardSetRepository.findById(id);
         if (optionalCardSet.isEmpty()) {
@@ -145,8 +148,12 @@ public class CardSetServiceImpl implements CardSetService {
         return cardSet;
     }
 
-    private User getUser(Authentication authentication) {
-        String email = authentication.getName();
+    private User getUser(long id) {
+        return userRepository.findById(id).orElseThrow(() -> new ResourceNotFound(id, User.class));
+    }
+
+    private User getUser(Authentication auth) {
+        String email = auth.getName();
 
         return userRepository.findByEmail(email).orElseThrow(() -> new ResourceNotFound(email, User.class));
     }
